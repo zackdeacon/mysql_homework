ee7653b71878dfa0b6bfa3b5413283885040d0a8
Zachary Deacon <zackdeacon347@gmail.com>
2020-07-14 21:09:16 -0700

    trying to add foreign keys

--- index.js
+++ index.js
@@ -59,26 +59,87 @@ var connection = mysql.createConnection({
     }
 
     function addDepartment() {
-
+        inquirer.prompt([{
+            type: "input",
+            message: "What is the name of the department that you would like to add?",
+            name: "departmentName"
+        }
+    ]).then(function({ departmentName }){
+            console.log(departmentName);
+            //need to add the new department to mysql
+        })
     }
 
     function addRole() {
+        inquirer.prompt([{
+            type: "input",
+            message: "What is the title of the role that you would like to add?",
+            name: "roleTitle"
+        },
+        {
+            type: "input",
+            message: "What is the annual salary for the role that you would like to add?",
+            name: "roleSalary"
+        }
+    ]).then(function({ roleTitle, roleSalary }){
+        console.log(roleTitle);
+        console.log(roleSalary);
+        //need to add the new role to mysql
 
+    })
     }
 
     function addEmployee() {
+        inquirer.prompt([{
+            type: "input",
+            message: "What is the first name of the employee that you would like to add?",
+            name: "empFName"
+        },
+        {
+            type: "input",
+            message: "What is the last name of the employee that you would like to add?",
+            name: "empLName"
+        }
+    ]).then(function({ empFName, empLName }){
+        console.log(empFName);
+        console.log(empLName);
+    })
 
     }
 
     function viewDepartment() {
+        var query = connection.query(
+            "SELECT * FROM employeesDB.department",
+            function(err, res) {
+              if (err) throw err;
+              console.table(res);
+              begin();
+            }
+          );
 
     }
 
     function viewRole() {
+        var query = connection.query(
+            "SELECT * FROM employeesDB.role",
+            function(err, res) {
+              if (err) throw err;
+              console.table(res);
+              begin();
+            }
+          );
 
     }
 
     function viewEmployee() {
+        var query = connection.query(
+            "SELECT * FROM employeesDB.employees",
+            function(err, res) {
+              if (err) throw err;
+              console.table(res);
+              begin();
+            }
+          );
 
     }
 
