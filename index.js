c637501e3e8d22c0b369c62279c1404a0e1adc6b
Zachary Deacon <zackdeacon347@gmail.com>
2020-07-14 20:03:02 -0700

    setting up initial JS

--- index.js
+++ index.js
@@ -20,3 +20,73 @@ var connection = mysql.createConnection({
       console.log("connected as id " + connection.threadId);
       begin();
     });
+
+    function begin() {
+        inquirer.prompt([{
+            
+            type: "list",
+            message: "What would you like to do?",
+            choices: ["add department", "add role", "add employee", "view department", "view role", "view employee", "update employee role", "quit"],
+            name: "initial"
+        }]).then(function({ initial }){
+            switch (initial) {
+                case "add department":
+                    addDepartment();
+                    break;
+                case "add role":
+                    addRole();
+                    break;
+                case "add employee":
+                    addEmployee();
+                    break;
+                case "view department":
+                    viewDepartment();
+                    break;
+                case "view role":
+                    viewRole();
+                    break;
+                case "view employee":
+                    viewEmployee();
+                    break;
+                case "update employee role":
+                    UpdateEmployeeRole();
+                    break;
+                case "quit":
+                    quit();
+                    break;
+            }
+        })
+    }
+
+    function addDepartment() {
+
+    }
+
+    function addRole() {
+
+    }
+
+    function addEmployee() {
+
+    }
+
+    function viewDepartment() {
+
+    }
+
+    function viewRole() {
+
+    }
+
+    function viewEmployee() {
+
+    }
+
+    function UpdateEmployeeRole() {
+
+    }
+
+    function quit() {
+        console.log("Goodbye!");
+        connection.end
+    }
--- schema.sql
+++ schema.sql
@@ -15,7 +15,7 @@ CREATE TABLE employees (
 CREATE TABLE role (
   id INT AUTO_INCREMENT NOT NULL,
   title VARCHAR(100) NOT NULL,
-  salary DECIMAL(10,4) NOT NULL,
+  salary DECIMAL(10,2) NOT NULL,
   department_id INT NULL,
   PRIMARY KEY (id)
 );
